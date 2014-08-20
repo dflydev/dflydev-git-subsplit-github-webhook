5cf9a355ef4179e05bb95fad88c556e4f40e2985
pitpit <damien.pitard@gmail.com>
2014-08-20 16:46:41 +0200

    Support payload submitted as http body in json. Allows to respond to Gitlab webhook and to new Github webhook content type (https://developer.github.com/v3/repos/hooks/#example)

--- web/subsplit-webhook.php
+++ web/subsplit-webhook.php
@@ -18,10 +18,19 @@ if (!in_array($_SERVER['REMOTE_ADDR'], $allowedIps)) {
     exit;
 }
 
-$body = $_POST['payload'];
+$headers = getallheaders();
+if (isset($headers['Content-Type']) && 'application/json' === $headers['Content-Type']) {
+    $body = file_get_contents('php://input');
+} else {
+    $body = $_POST['payload'];
+}
+
+if (!$body) {
+    header('HTTP/1.1 400 Bad Request', true, 400);
+    exit;
+}
 
 $redis = new Predis\Client();
-
 $redis->lpush('dflydev-git-subsplit:incoming', $body);
 
 echo "Thanks.\n";
